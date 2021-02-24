12f01254748d5f0a709d556461deaa5771a2fa83
Lucas <lucaspedrazoli@gmail.com>
2021-02-24 12:53:39 -0300

    add gradient colors

--- SlideToReveal/AnimatedMaskLabel.swift
+++ SlideToReveal/AnimatedMaskLabel.swift
@@ -8,6 +8,12 @@ class AnimatedMaskLabel: UIView {
     let gradientLayer = CAGradientLayer()
     gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
     gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+    let colors = [
+      UIColor.black.cgColor,
+      UIColor.white.cgColor,
+      UIColor.black.cgColor
+    ]
+    gradientLayer.colors = colors
     return gradientLayer
   }()
   
