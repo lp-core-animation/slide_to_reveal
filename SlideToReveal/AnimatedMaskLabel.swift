d3e09ec5139cd61ce83fe6b64c54ab74290a8fb3
Lucas <lucaspedrazoli@gmail.com>
2021-02-24 12:54:58 -0300

    add gradient color location frame

--- SlideToReveal/AnimatedMaskLabel.swift
+++ SlideToReveal/AnimatedMaskLabel.swift
@@ -8,12 +8,12 @@ class AnimatedMaskLabel: UIView {
     let gradientLayer = CAGradientLayer()
     gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
     gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
-    let colors = [
-      UIColor.black.cgColor,
-      UIColor.white.cgColor,
-      UIColor.black.cgColor
-    ]
+    let colors = [UIColor.black.cgColor,
+                  UIColor.white.cgColor,
+                  UIColor.black.cgColor]
     gradientLayer.colors = colors
+    let locations: [NSNumber] = [0.25, 0.5, 0.75]
+    gradientLayer.locations = locations
     return gradientLayer
   }()
   
